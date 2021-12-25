4f029e3cd42e0e29c2ad3e20b8f4c068dbad5760
Ebubechukwu Dimobi <dimobiblaise@gmail.com>
2021-12-25 23:22:50 +0300

    update dataset

--- MySentimentsDataCreator.playground/Contents.swift
+++ MySentimentsDataCreator.playground/Contents.swift
@@ -1,41 +1,33 @@
 import Cocoa
 import CreateML
 
-
-let dataSetFilePath = URL(fileURLWithPath: "/Users/ebubed/Desktop/Masters_first_year/Practical/Sentimental/twitter-sanders-apple3.csv")
+//1. Import Data as .csv
+let dataSetFilePath = URL(fileURLWithPath: "/Users/ebubed/Desktop/Masters_first_year/Practical/Sentimental/DataSett.csv")
 let data = try MLDataTable(contentsOf: dataSetFilePath)
 
-// split training data and testingData to 80% to random data
-let(trainigData, testingData) = data.randomSplit(by: 0.8, seed: 6)
-
-//create a machine learning module using ML which uses NLP under the hood
+//2. Preparing Data for Training and Evaluation
+/// split training data and testingData to 80% to random data
+let(trainigData, testingData) = data.randomSplit(by: 0.8, seed: 5)
+///create a machine learning module using ML which uses NLP under the hood
 let sentimentClassifier = try MLTextClassifier(trainingData: trainigData, textColumn: "text", labelColumn: "class")
-
 let trainingMetrics = sentimentClassifier.evaluation(on: trainigData, textColumn: "text", labelColumn: "class")
-let trainingAccuracy = (1.0 - trainingMetrics.classificationError) * 100
 
-// test our data
+//3. Evaluating the Classifier’s Accuracy
+let trainingAccuracy = (1.0 - trainingMetrics.classificationError) * 100
+/// test our data
 let evaluationMetrics = sentimentClassifier.evaluation(on: testingData, textColumn: "text", labelColumn: "class")
 let evaluationAccuracy = (1.0 - evaluationMetrics.classificationError) * 100
 
-//create metadata
-
+//4. Saving the Model
+///create metadata
 let metadata = MLModelMetadata(author: "Dimobi Ebubechukwu", shortDescription: "A model trained to classify sentiments", license: nil, version: "1.0.0")
-
-
-// write to url
+/// write to file path
 let destinationPath = URL(fileURLWithPath: "/Users/ebubed/Desktop/Masters_first_year/Practical/Sentimental/TextSentimentClassifier")
-
 try sentimentClassifier.write(to: destinationPath, metadata: metadata)
 
-
-//test
-
+//trials
 try sentimentClassifier.prediction(from: "I hate you")
-
 try sentimentClassifier.prediction(from: "@apple i hate you")
 try sentimentClassifier.prediction(from: "@apple Today is great")
-
 try sentimentClassifier.prediction(from: "you are annoing but i like you")
-
 try sentimentClassifier.prediction(from: "i just found the best restaurant ever")
