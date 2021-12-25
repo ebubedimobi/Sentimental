660d7413b020b3238347b6ff71882820c1748369
Ebubechukwu Dimobi <dimobiblaise@gmail.com>
2021-12-25 16:59:03 +0300

    Initial Commit

---
+++ Sentimental/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sentimental
+//
+//  Created by Ebubechukwu Dimobi on 25.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
