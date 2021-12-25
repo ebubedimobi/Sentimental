4170564c58ebcba46b8089e3ccb3f3db35507420
Ebubechukwu Dimobi <dimobiblaise@gmail.com>
2021-12-25 20:19:44 +0300

    made first changes

---
+++ Sentimental/Manager.swift
@@ -0,0 +1,51 @@
+//
+//  Manager.swift
+//  Sentimental
+//
+//  Created by Ebubechukwu Dimobi on 25.12.2021.
+//
+
+import Foundation
+
+class Manager {
+    
+    let textClassifier = try? TextSentimentClassifier(configuration: .init())
+    
+    func getPrediction(on text: String) -> TextClassifierResult? {
+        
+        if let predictedText = try? textClassifier?.prediction(text: text).label {
+            return TextClassifierResult(rawValue: predictedText)
+        } else {
+            return nil
+        }
+    }
+}
+
+
+enum TextClassifierResult: String {
+    case pos = "Pos"
+    case neutral = "Neutral"
+    case negative = "Neg"
+    
+    var AnimationName: String {
+        switch self {
+        case .pos:
+            return ["cat_happy", "mascot", "star"].randomElement() ?? "cat"
+        case .neutral:
+            return ["emoji", "swinging_neutral"].randomElement() ?? "emoji"
+        case .negative:
+            return ["cat_sad", "emoji_sad", "swinging_sad", "heart"].randomElement() ?? "heart" 
+        }
+    }
+    
+    var outputText: String {
+        switch self {
+        case .pos:
+            return "Positive: You are feeling good today!"
+        case .neutral:
+            return "Neutral: Don't worry it will get better"
+        case .negative:
+            return "Negative: I am scared! Who are you??!"
+        }
+    }
+}
--- Sentimental/ViewController.swift
+++ Sentimental/ViewController.swift
@@ -6,14 +6,110 @@
 //
 
 import UIKit
+import SnapKit
+import Lottie
 
 class ViewController: UIViewController {
+    
+   
+    private var result: TextClassifierResult?
+    
+    private let manager = Manager()
+    
+    private let animationView: AnimationView = AnimationView(frame: .zero)
+    
+    private lazy var resultLabel: UILabel = {
+        let label = UILabel(frame: .zero)
+        label.font = .systemFont(ofSize: 14.0)
+        return label
+    }()
+    
+    private lazy var inputTextView: UITextView = {
+        let textView = UITextView(frame: .zero)
+        return textView
+    }()
+    
+    private lazy var predictButton: UIButton = {
+        let button = UIButton(frame: .zero)
+        button.setTitle("Predict", for: .normal)
+        button.layer.cornerRadius = 20.0
+        button.titleLabel?.font = .systemFont(ofSize: 18.0)
+        button.addTarget(self, action: #selector(didTapPredictButton(_:)), for: .touchUpInside)
+
+        return button
+    }()
+    
+    @objc
+    private func didTapPredictButton(_ sender: UIButton) {
+        guard
+            let text = inputTextView.text,
+            !text.isEmpty else {
+                return
+            }
+        
+        if let result = manager.getPrediction(on: text) {
+            resultLabel.text = result.outputText
+            resultLabel.sizeToFit()
+            setAnimationWithResult(result)
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.title = "Sentimetal"
+        makeConstraints()
         // Do any additional setup after loading the view.
+    }
+    
+    private func makeConstraints() {
+        [animationView, resultLabel, inputTextView, predictButton].forEach { view.addSubview($0) }
+        
+        animationView.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.topMargin).inset(16.0)
+            make.leading.trailing.equalToSuperview().inset(16.0)
+            make.bottom.lessThanOrEqualTo(view.snp.centerY)
+            make.centerX.equalToSuperview()
+        }
+        
+        resultLabel.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview().inset(16.0)
+            make.centerX.equalToSuperview()
+            make.top.equalTo(animationView.snp.bottom).offset(16.0)
+        }
+        
+        inputTextView.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview().inset(16.0)
+            make.height.equalTo(50.0)
+            make.bottom.equalTo(predictButton.snp.bottom).offset(30.0)
+        }
+        
+        predictButton.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview().inset(16.0)
+            make.height.equalTo(48.0)
+            make.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom).inset(16.0)
+        }
     }
 
 
 }
 
+
+extension ViewController {
+    
+    func setAnimationWithResult(_ result: TextClassifierResult) {
+        if animationView.isAnimationPlaying {
+            animationView.stop()
+        }
+        animationView.isHidden = false
+        animationView.animation = lottieAnimationWithResult(result)
+        animationView.loopMode = .loop
+        animationView.play()
+        animationView.backgroundBehavior = .pauseAndRestore
+        self.result = result
+    }
+    
+    private func lottieAnimationWithResult(_ result: TextClassifierResult) -> Lottie.Animation? {
+        return Lottie.Animation.named(result.AnimationName)
+    }
+   
+}
